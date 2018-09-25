5effeec7ce9012ce83ae334c0b2e103c7f0fbbf2
shuxnhs <610087273@qq.com>
2018-09-26 01:06:21 +0800

    PSR-0基础框架,自动载入

---
+++ PSR-0/IMooc/Object.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 何晓宏
+ * Date: 2018/9/25
+ * Time: 23:40
+ */
+    namespace IMooc;
+
+    class Object{
+
+        static function test(){
+
+
+        }
+
+    }
+
+
+    /*下面不能有可执行代码
+        echo “”；
+    */
