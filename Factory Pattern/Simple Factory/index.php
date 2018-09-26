179cc991acef878dcaef0dee376424c64d2b8b25
shuxnhs <610087273@qq.com>
2018-09-26 21:01:31 +0800

    simplefactory

---
+++ Factory Pattern/Simple Factory/index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 何晓宏
+ * Date: 2018/9/25
+ * Time: 20:01
+ */
+
+//定义文件目录
+define('BASEDIR', __DIR__);
+include BASEDIR.'/IMooc/Loader.php';
+spl_autoload_register('\\IMooc\\Loader::autoload');
+
+
+$phone1=\IMooc\Factory::buyIphone('Iphone');
+$phone1->buy();
+
+$phone2=\IMooc\Factory::buyIphone('Huawei');
+$phone2->buy();
+
+
+
+
